0ee38b94053293b7403b185de16e49ca45837fd1
niels voogt <n.voogt@gmail.com>
2020-10-25 16:44:47 +0100

    Cleaned up a bit

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,11 @@
 const functions = require("firebase-functions");
+const algoliaSearch = require("algoliasearch");
+const admin = require("firebase-admin");
 
-// Initialize Algolia, requires installing Algolia dependencies:
-// https://www.algolia.com/doc/api-client/javascript/getting-started/#install
-//
-// App ID and API Key are stored in functions config variables
+// Initialize Algolia, App ID and API Key are stored in functions config variables
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
-
-const ALGOLIA_INDEX_NAME = "users";
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const algoliaSearch = require("algoliasearch");
-const admin = require("firebase-admin");
 
 const algoliaClient = algoliaSearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 const algoliaIndex = algoliaClient.initIndex("users");
